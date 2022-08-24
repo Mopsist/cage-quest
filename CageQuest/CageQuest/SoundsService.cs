5563b2fad445e2632eb4b1526edeaaa3a3f48146
Mikita Sauko <Mikita.Sauko@itechart-group.com>
2022-08-24 15:58:57 +0300

    Changed wav file to mp3 and added convertion method

--- CageQuest/CageQuest/SoundsService.cs
+++ CageQuest/CageQuest/SoundsService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Media;
+using NAudio.Wave;
 
 namespace CageQuest
 {
@@ -9,8 +10,12 @@ namespace CageQuest
 
         public SoundsService()
         {
+            var mp3Path = AppDomain.CurrentDomain.BaseDirectory + "Resources\\scott-buckley-i-walk-with-ghosts.mp3";
+            var wavPath = AppDomain.CurrentDomain.BaseDirectory + "Resources\\scott-buckley-i-walk-with-ghosts.wav";
+            ConvertMp3ToWav(mp3Path, wavPath);
+
             _player = new SoundPlayer();
-            _player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "Resources\\scott-buckley-i-walk-with-ghosts.wav";
+            _player.SoundLocation = wavPath;
         }
 
         public void PlayBackgroundMusic()
@@ -21,6 +26,13 @@ namespace CageQuest
         public void StopBackgroundMusic()
         {
             _player.Stop();
+        }
+
+        private static void ConvertMp3ToWav(string _inPath_, string _outPath_)
+        {
+            using var mp3 = new Mp3FileReader(_inPath_);
+            using var pcm = WaveFormatConversionStream.CreatePcmStream(mp3);
+            WaveFileWriter.CreateWaveFile(_outPath_, pcm);
         }
     }
 }
